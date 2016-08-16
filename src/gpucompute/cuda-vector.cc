ce1406cc5d239752f542ab56b0bdd11996d3f962
Mohammad Abdelaziz Gowayyed <gowayyed@comet-ln2.sdsc.edu>
2016-08-16 08:37:53 -0700

    fixed compilation bug

--- src/gpucompute/cuda-vector.cc
+++ src/gpucompute/cuda-vector.cc
@@ -823,41 +823,7 @@ void CuVector<Real>::Destroy() {
 }
 
 
-template<>
-void CuVectorBase<float>::CopyFromVec(const CuVectorBase<float> &src) {
-  KALDI_ASSERT(src.Dim() == dim_);
-#if HAVE_CUDA == 1
-  if (CuDevice::Instantiate().Enabled()) {
-    if (dim_ == 0) return;
-    Timer tim;
-    CU_SAFE_CALL(cudaMemcpy(data_, src.data_, src.dim_ * sizeof(float), cudaMemcpyDeviceToDevice));
-    CuDevice::Instantiate().AccuProfile(__func__, tim.Elapsed());
-  } else
-  #endif
-  {
-    memcpy(static_cast<void*>(data_), static_cast<void*>(src.data_),
-           dim_ * sizeof(float));
-  }
-}
 
-template<>
-void CuVectorBase<double>::CopyFromVec<double>(const CuVectorBase<double> &src) {
-  KALDI_ASSERT(src.Dim() == dim_);
-#if HAVE_CUDA == 1
-  if (CuDevice::Instantiate().Enabled()) {
-    if (dim_ == 0) return;
-    Timer tim;
-    CU_SAFE_CALL(cudaMemcpy(data_, src.data_, src.dim_ * sizeof(double), cudaMemcpyDeviceToDevice));
-    CuDevice::Instantiate().AccuProfile(__func__, tim.Elapsed());
-  } else
-  #endif
-  {
-    memcpy(static_cast<void*>(data_), static_cast<void*>(src.data_),
-           dim_ * sizeof(double));
-  }
-}
-
-/*
 template<typename Real>
 void CuVectorBase<Real>::CopyFromVec(const CuVectorBase<Real> &src) {
   KALDI_ASSERT(src.Dim() == dim_);
@@ -875,11 +841,7 @@ void CuVectorBase<Real>::CopyFromVec(const CuVectorBase<Real> &src) {
   }
 }
 
-template
-void CuVectorBase<float>::CopyFromVec(const CuVectorBase<float> &src);
-template
-void CuVectorBase<double>::CopyFromVec(const CuVectorBase<double> &src);
-*/
+
 
 template<typename Real>
 void CuVectorBase<Real>::SetZero() {
